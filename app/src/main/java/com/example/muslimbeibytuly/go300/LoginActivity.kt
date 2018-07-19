455fe0197e68abb0f62337bf88e07ddf4cf9809c
muslimbeibytuly <muslimbeibytuly@gmail.com>
2018-07-20 00:20:12 +0600

    WIP on login activity

--- app/src/main/java/com/example/muslimbeibytuly/go300/LoginActivity.kt
+++ app/src/main/java/com/example/muslimbeibytuly/go300/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.example.muslimbeibytuly.go300
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val usernameEditText = findViewById<EditText>(R.id.username_edit_text)
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton?.setOnClickListener {
+            val username = usernameEditText.text
+        }
     }
 }
